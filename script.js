00de1ad6aa71465af0271539403a3549761c56eb
chinfuk <kidtalbot@gmail.com>
2021-10-24 11:50:04 +0100

    setInterval added and tyided

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function hang(){
-    document.getElementById("timer").innerHTML = "Hang!";
-    setTimeout(function(){document.getElementById("timer").innerHTML = "Rest!"; }, 3000);
+    var interval = 500;
+    var hangtime = 7;
+    var resttime = 3;
 
+    let ht = setInterval(() => {
+                document.getElementById("timer").innerHTML = `Hang! ${hangtime}`;
+                hangtime--;
+                if (hangtime < 0 ){
+        
+                    clearInterval(ht);
+        }
+
+
+        
+    }, interval);
 }
