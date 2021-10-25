95e21f73bddc18183fb758224114c70968bb0e37
Ki <kidtalbot@gmail.com>
2021-10-25 09:57:58 +0100

    testing uot the sets feature

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
+
+    var setno = 3;
+    var interval = 200;
+    var hangtime = 3;
+    var resttime = 3;
+    function operate(){
+        setInterval(() => {
+           if(setno > 0){
+               hang()
+               if(hangtime < 0){
+                    rest()
+                    if(resttime < 0){
+                        sets()
+                        console.log(setno)
+                    }
+               }
+           } 
+        }, interval);
+    }
+
+
+
 function hang(){
-    document.getElementById("timer").innerHTML = "Hang!";
-    setTimeout(function(){document.getElementById("timer").innerHTML = "Rest!"; }, 3000);
+    document.getElementById("timer").innerHTML = `Hang! ${hangtime}`;
+    hangtime--;
+}
+
+function rest(){
+    document.getElementById("timer").innerHTML = `Rest! ${resttime}`;
+    resttime--;
+}    
+
+function sets(){
+    setno--;
 
 }
+        
+        
+
+
